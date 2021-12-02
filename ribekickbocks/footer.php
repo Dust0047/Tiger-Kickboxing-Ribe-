20a32a0359d7c0305d03c388486fcf45146df211
Dust0047 <72190587+Dust0047@users.noreply.github.com>
2021-12-02 13:24:15 +0100

    Add files via upload

---
+++ ribekickbocks/footer.php
@@ -0,0 +1,29 @@
+<?php wp_footer(); ?>
+
+
+<footer>
+            <div class="footer-wrap">
+                <div class="footer-logo">
+                    <a href=""><img class="img/logo.png" src="<?php the_field ("logo") ?>" alt="">
+
+                </div>
+
+
+            
+
+                <div class="footer-social">
+                <a href="https://www.facebook.com/tigerribe" target="_blank"
+                                class="fa fa-facebook"></a>
+                 <a href="https://twitter.com/" target="_blank" class="fa fa-twitter"></a>
+            
+                 <a href="https://instagram.com/tigerkickboxingribe" target="_blank"
+                                class="fa fa-instagram"></a>
+
+                <a href="https://www.youtube.com/watch?v=kgNg8JkM8c0/" target="_blank"
+                                class="fa fa-youtube"></a>                </div>
+            </div>
+        </footer>
+
+
+</body>
+</html>
---
+++ ribekickbocks/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<?php wp_head(); ?>
+
+
+<head>
+        <meta charset="UTF-8">
+        <title>Tiger Kickboxing Ribe</title>
+        <script src="https://kit.fontawesome.com/1edaaf2a3d.js" crossorigin="anonymous"></script>
+        
+    </head>
+  
+
+        <nav>
+        <div class="topnav">
+        <?php wp_nav_menu(array("theme-location" => "main-menu")) ?>
+     
+
+        <!-- <a class="active" href="#home">Home</a>
+        <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="about.php">About</a> -->
+
+
+</div>
+</nav>
+   
+    </div>
+  
+        </header>
+
+        
